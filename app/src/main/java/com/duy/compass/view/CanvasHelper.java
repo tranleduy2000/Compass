bd84354adc086adfb728bd2676daa1d8c9633c7e
tran_le_duy <tranleduy1233@gmail.com>
2017-10-15 16:03:41 +0700

    draw dir

--- app/src/main/java/com/duy/compass/view/CanvasHelper.java
+++ app/src/main/java/com/duy/compass/view/CanvasHelper.java
@@ -6,84 +6,200 @@ import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.text.TextPaint;
 
 public class CanvasHelper {
-    private static int f9236a = 5;
-    private static int mDirectionWidth = 10;
-    private static int mLineWidth = 7;
+    private final TextPaint mNumberPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+    private float mPixelScale;
+    private int mWidth, mHeight;
+    private Point mCenter;
+    private float mUnitPadding;
+    private float mClockNumberSize = 25;
+    private float mDirectionTextSize = 25f;
 
-    public static void drawClock(Canvas canvas) {
-        int width = canvas.getWidth();
-        int height = canvas.getHeight();
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        Point center = new Point(width / 2, height / 2);
-        float lineLength = ((float) width) / 1200.0f;
-
-        CanvasHelper.drawClock(canvas, paint, center, lineLength);
-        CanvasHelper.drawClock1(canvas, paint, center, lineLength);
-        CanvasHelper.drawDirection(canvas, center, lineLength);
+    public CanvasHelper() {
     }
 
-    private static void drawDirection(Canvas canvas, Point center, float lineLength) {
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth((float) mDirectionWidth);
-        Path path = new Path();
-        float toRadians = (float) Math.toRadians(270.0d);
-        float cos = (float) Math.cos((double) toRadians);
-        toRadians = (float) Math.sin((double) toRadians);
-        float f2 = (350.0f * lineLength) * cos;
-        float f3 = (350.0f * lineLength) * toRadians;
-        cos *= 450.0f * lineLength;
-        lineLength = (lineLength * 450.0f) * toRadians;
-        path.moveTo(((float) center.x) + f2, ((float) center.y) + f3);
-        path.lineTo(((float) center.x) + cos, lineLength + ((float) center.y));
-        canvas.drawPath(path, paint);
+    public void draw(Canvas canvas) {
+        mWidth = canvas.getWidth();
+        mHeight = canvas.getHeight();
+        mPixelScale = ((float) mWidth) / 1000.0f;
+        mCenter = new Point(mWidth / 2, mHeight / 2);
+        mUnitPadding = realWidth(5);
+        mDirectionTextSize = realWidth(40f);
+
+        initPaint();
+
+        //draw background
+        canvas.drawRGB(0, 0, 0);
+
+        drawCircle(canvas);
+        drawClock(canvas);
+        drawDirection(canvas);
     }
 
-    private static void drawClock(Canvas canvas, Paint paint, Point point, float f) {
+    private void initPaint() {
+        mNumberPaint.setTextSize(realWidth(mClockNumberSize));
+        mNumberPaint.setColor(Color.WHITE);
+    }
+
+    private void drawCircle(Canvas canvas) {
+        float radius = realWidth(430);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.GRAY);
         paint.setStyle(Style.STROKE);
-        paint.setStrokeWidth((float) f9236a);
+        paint.setStrokeWidth(realWidth(3));
+
+        canvas.drawCircle(mCenter.x, mCenter.y, radius, paint);
+
+        paint.setColor(Color.DKGRAY);
+
+        mNumberPaint.setTextSize(realWidth(mClockNumberSize));
+        Paint.FontMetrics fm = mNumberPaint.getFontMetrics();
+        float fontHeight = fm.bottom - fm.top + fm.leading;
+
+        float strokeWidth = 20 + fontHeight;
+        paint.setStrokeWidth(realWidth(strokeWidth));
+
+        radius = realWidth(350 - strokeWidth / 2.0f - mUnitPadding);
+        canvas.drawCircle(mCenter.x, mCenter.y, radius, paint);
+    }
+
+    private void drawClock(Canvas canvas) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        drawClock(canvas, paint, mCenter);
+        drawClock1(canvas, paint, mCenter);
+        drawNumber(canvas);
+    }
+
+    private void drawDirection(Canvas canvas) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(realWidth(5));
         Path path = new Path();
-        double degreeStep = 2.5d;
-        for (double step = 0.0f; step < 2 * Math.PI; step = (step + Math.toRadians(degreeStep))) {
+
+        float radian = (float) Math.toRadians(270.0d);
+
+        float cos = (float) Math.cos((double) radian);
+        float sin = (float) Math.sin((double) radian);
+
+        float x = realWidth(320) * cos;
+        float y = realWidth(320) * sin;
+        path.moveTo(((float) mCenter.x) + x, ((float) mCenter.y) + y);
+
+        x = realWidth(400) * cos;
+        y = realWidth(400) * sin;
+        path.lineTo(x + ((float) mCenter.x), y + ((float) mCenter.y));
+        canvas.drawPath(path, paint);
+
+
+        //draw triangle
+        paint.setStyle(Style.FILL);
+        paint.setColor(Color.WHITE);
+
+        x = mCenter.x;
+        y = mCenter.y - realWidth(430 + mUnitPadding * 2);
+        path.reset();
+        float length = realWidth(20);
+        path.lineTo(x - length / 2.0f, y - length);
+        path.lineTo(x + length / 2.0f, y - length);
+        path.lineTo(x, y);
+        path.lineTo(x - length / 2.0f, y - length);
+        canvas.drawPath(path, paint);
+
+        //draw direction N S E W
+        //N = 0, E = 90, S = 180, W = 270
+        mNumberPaint.setTextSize(realWidth(mClockNumberSize));
+        Paint.FontMetrics fm = mNumberPaint.getFontMetrics();
+        float fontHeight = fm.bottom - fm.top + fm.leading;
+        float radius = 330 - fontHeight - mUnitPadding ;
+        drawText(canvas, 270, "N", mDirectionTextSize, radius);
+        drawText(canvas, 0, "E", mDirectionTextSize, radius);
+        drawText(canvas, 90, "S", mDirectionTextSize, radius);
+        drawText(canvas, 180, "W", mDirectionTextSize, radius);
+    }
+
+    private void drawClock(Canvas canvas, Paint paint, Point center) {
+        paint.setColor(Color.GRAY);
+        paint.setStyle(Style.STROKE);
+        paint.setStrokeWidth(realWidth(3));
+        Path path = new Path();
+        float degreeStep = 2.5f;
+        for (float step = 0.0f; step < 2 * Math.PI; step += Math.toRadians(degreeStep)) {
             float cos = (float) Math.cos(step);
             float sin = (float) Math.sin(step);
 
+            float x = realWidth(350) * cos;
+            float y = realWidth(350) * sin;
+            path.moveTo(x + ((float) center.x), y + ((float) center.y));
 
-            float f3 = (350.0f * f) * cos;
-            float f4 = (350.0f * f) * sin;
-            path.moveTo(f4 + (float) point.y, f3 + (float) point.x);
-
-            cos *= 380.0f * f;
-            sin *= 380.0f * f;
-            path.lineTo(cos + (float) point.x, sin + (float) point.y);
+            x = realWidth(380) * cos;
+            y = realWidth(380) * sin;
+            path.lineTo(x + ((float) center.x), y + ((float) center.y));
         }
         canvas.drawPath(path, paint);
     }
 
-    private static void drawClock1(Canvas canvas, Paint paint, Point center, float lineLength) {
+    private float realWidth(float width) {
+        return width * mPixelScale;
+    }
+
+    private void drawClock1(Canvas canvas, Paint paint, Point center) {
         paint.setColor(Color.WHITE);
-        paint.setStrokeWidth((float) mLineWidth);
+        paint.setStrokeWidth(realWidth(5));
         Path path = new Path();
-        double degreeStep = 30.0d;
-        for (double step = 0.0f; step < 2 * Math.PI; step += Math.toRadians(degreeStep)) {
+        float degreeStep = 30.0f;
+        for (float step = 0.0f; step < 2 * Math.PI; step += Math.toRadians(degreeStep)) {
             float cos = (float) Math.cos(step);
             float sin = (float) Math.sin(step);
 
-            float r1 = 350.0f;
-            float f3 = (r1 * lineLength) * cos;
-            float f4 = (r1 * lineLength) * sin;
-            path.moveTo(f3 + ((float) center.x), f4 + ((float) center.y));
+            float x = realWidth(330) * cos;
+            float y = realWidth(330) * sin;
+            path.moveTo(x + ((float) center.x), y + ((float) center.y));
 
-            float r2 = 400.0f;
-            cos *= r2 * lineLength;
-            sin *= r2 * lineLength;
+            cos *= realWidth(380);
+            sin *= realWidth(380);
             path.lineTo(cos + ((float) center.x), sin + ((float) center.y));
         }
         canvas.drawPath(path, paint);
     }
 
+    private void drawNumber(Canvas canvas) {
+        float radius = 330;
+        drawText(canvas, 300.0f, "30", mClockNumberSize, radius);
+        drawText(canvas, 330.0f, "60", mClockNumberSize, radius);
+        drawText(canvas, 360.0f, "90", mClockNumberSize, radius);
+        drawText(canvas, 30.0f, "120", mClockNumberSize, radius);
+        drawText(canvas, 60.0f, "150", mClockNumberSize, radius);
+        drawText(canvas, 90.0f, "180", mClockNumberSize, radius);
+        drawText(canvas, 120.0f, "210", mClockNumberSize, radius);
+        drawText(canvas, 150.0f, "240", mClockNumberSize, radius);
+        drawText(canvas, 180.0f, "270", mClockNumberSize, radius);
+        drawText(canvas, 210.0f, "300", mClockNumberSize, radius);
+        drawText(canvas, 240.0f, "330", mClockNumberSize, radius);
+    }
 
+    private void drawText(Canvas canvas, float degree, String text, float textSize, float radius) {
+        mNumberPaint.setTextSize(textSize);
+        Paint.FontMetrics fm = mNumberPaint.getFontMetrics();
+        float height = fm.bottom - fm.top + fm.leading;
+
+        float cos = (float) Math.cos(Math.toRadians(degree));
+        float sin = (float) Math.sin(Math.toRadians(degree));
+
+        float x = (cos * realWidth(radius)) + mCenter.x;
+        float y = (sin * realWidth(radius)) + mCenter.y;
+
+        canvas.drawPoint(x, y, mNumberPaint);
+        canvas.drawPoint(mCenter.x, mCenter.y, mNumberPaint);
+
+        canvas.save();
+
+        canvas.translate(x, y);
+        canvas.rotate(90.0f + degree);
+        canvas.drawText(text, -mNumberPaint.measureText(text) / 2.0f, height, mNumberPaint);
+
+        canvas.restore();
+    }
 }
--- app/src/main/java/com/duy/compass/view/CompassView.java
+++ app/src/main/java/com/duy/compass/view/CompassView.java
@@ -2,27 +2,18 @@ package com.duy.compass.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.View;
 
 /**
  * Created by Duy on 10/15/2017.
  */
 
-public class CompassView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
-    private Thread mCurrentThread;
+public class CompassView extends View {
+    CanvasHelper mCanvasHelper;
     private boolean mIsPortrait;
-    private volatile boolean mIsActive;
-
-    private Point mCenter = new Point();
-    private float mClockRadius = 0f;
-    private int mLineWidth = 7;
 
     public CompassView(Context context) {
         super(context);
@@ -40,9 +31,10 @@ public class CompassView extends SurfaceView implements SurfaceHolder.Callback, 
     }
 
     private void init(Context context) {
-        getHolder().addCallback(this);
+//        getHolder().addCallback(this);
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
         this.mIsPortrait = ((float) displayMetrics.heightPixels) / ((float) displayMetrics.widthPixels) > 1.4f;
+        mCanvasHelper = new CanvasHelper();
     }
 
     @Override
@@ -69,73 +61,8 @@ public class CompassView extends SurfaceView implements SurfaceHolder.Callback, 
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        this.mCenter.set(w / 2, h / 2);
-        this.mClockRadius = w / 2;
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        this.mIsActive = true;
-        this.mCurrentThread = new Thread(this);
-        this.mCurrentThread.start();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        this.mIsActive = false;
-        if (this.mCurrentThread != null) {
-            try {
-                this.mCurrentThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            this.mCurrentThread = null;
-        }
-    }
-
-    @Override
-    public void run() {
-        while (mIsActive) {
-            long currentTimeMillis = System.currentTimeMillis();
-            try {
-                Canvas canvas = getHolder().lockCanvas();
-                if (canvas != null) {
-                    CanvasHelper.drawClock(canvas);
-//                drawNumber(canvas);
-//                drawDirection(canvas);
-                }
-                if (canvas != null) {
-                    try {
-                        getHolder().unlockCanvasAndPost(canvas);
-                    } catch (Throwable e) {
-//                        C0888a.m3420a(e);
-                    }
-                }
-                long floor = ((long) Math.floor(16.0d)) - (System.currentTimeMillis() - currentTimeMillis);
-                if (floor > 0) {
-                    try {
-                        Thread.sleep(floor);
-                    } catch (InterruptedException e2) {
-                        e2.printStackTrace();
-                    }
-                }
-            } catch (Exception e) {
-
-            }
-        }
-    }
-
-    private void drawLine(Canvas canvas) {
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.WHITE);
-        paint.setStrokeWidth(mLineWidth);
-        float lineLength = ((float) canvas.getWidth()) / 1200.0f;
-        CanvasHelper.drawClock(canvas, mCenter, mClockRadius, lineLength, paint);
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mCanvasHelper.draw(canvas);
     }
 }
